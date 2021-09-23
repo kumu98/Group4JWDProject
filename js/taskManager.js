bd7f3096c3e08b892b33d9b2195b8250ac94537d
kumu <kumudhini.j@gmail.com>
2021-09-23 15:48:02 +1000

    working on task7

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,3 +1,32 @@
+function createTaskHtml (name, description, assignedTo, dueDate, status) {
+const html = `<li class="card" style="min-width: 50vw">
+<div class="card-body">
+    <h5 class="card-title">${name}</h5>
+    <p class="card-text">
+        ${description}
+    </p>
+    <p class="card-text">${assignedTo} To</p>
+    <p class="card-text">${dueDate}</p>
+    <div class="card-footer row">
+        <div class="col-6">
+            <p class="card-text"><b>${status}</b></p>
+        </div>
+        <div class="col-3">
+            <button class="btn btn-outline-success done-button">
+                Done
+            </button>
+        </div>
+        <div class="col-3">
+            <button class="btn btn-outline-danger delete-button">
+                Delete
+            </button>
+        </div>
+    </div>
+   </div>
+</li>`;
+return html;
+}
+
 class TaskManager {
     constructor(currentId=0){
         this._tasks = [];
@@ -14,7 +43,26 @@ class TaskManager {
         }
         this._tasks.push(newTask);
     }
-    // get tasks(){
-    //     return this._tasks;
-    // }
+
+    render(){
+        const tasksHtmlList = [];
+        
+        for(let i=0; i<this._tasks.length; i++){
+            let taskHtml = createTaskHtml (
+               tasks.name,
+                tasks.description,
+                tasks.assignedTo,
+                tasks.dueDate,
+                tasks.status
+            ); 
+        tasksHtmlList.push(taskHtml);
+        console.log(tasksHtmlList);
+        const mytask = document.getElementById('mytask');
+        mytask.innerHTML = taskHtml;
+        }
+
+        
+        
+    }
+    
 }
