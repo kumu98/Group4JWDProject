b81e4dd8d2b767173091da213698ea6ca1b54ccb
kumu <kumudhini.j@gmail.com>
2021-09-23 18:26:52 +1000

    task7 completed

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,11 +1,11 @@
 function createTaskHtml (name, description, assignedTo, dueDate, status) {
-const html = `<li class="card" style="min-width: 50vw">
+let html = `<li class="card" style="min-width: 50vw">
 <div class="card-body">
     <h5 class="card-title">${name}</h5>
     <p class="card-text">
         ${description}
     </p>
-    <p class="card-text">${assignedTo} To</p>
+    <p class="card-text">${assignedTo} </p>
     <p class="card-text">${dueDate}</p>
     <div class="card-footer row">
         <div class="col-6">
@@ -27,6 +27,7 @@ const html = `<li class="card" style="min-width: 50vw">
 return html;
 }
 
+
 class TaskManager {
     constructor(currentId=0){
         this._tasks = [];
@@ -40,29 +41,31 @@ class TaskManager {
             assignedTo: assignedTo,
             dueDate: dueDate,
             status: status,
-        }
+        };
         this._tasks.push(newTask);
     }
 
-    render(){
-        const tasksHtmlList = [];
-        
-        for(let i=0; i<this._tasks.length; i++){
-            let taskHtml = createTaskHtml (
-               tasks.name,
-                tasks.description,
-                tasks.assignedTo,
-                tasks.dueDate,
-                tasks.status
-            ); 
-        tasksHtmlList.push(taskHtml);
-        console.log(tasksHtmlList);
-        const mytask = document.getElementById('mytask');
-        mytask.innerHTML = taskHtml;
-        }
+   render() {
+       let tasksHtmlList = [];
+       for (let i=0; i< this._tasks.length; i++){
+        let currentTask = this._tasks[i];
+           let taskHtml = createTaskHtml (
+               currentTask.name,
+               currentTask.description,
+               currentTask.assignedTo,
+               currentTask.dueDate,
+               currentTask.status
+           )
+           tasksHtmlList.push(taskHtml);
+       }
+       const mycard = document.getElementById('mycard');
+       mycard.innerHTML=tasksHtmlList;
 
-        
-        
-    }
-    
+   } 
+
+      
 }
+
+
+          
+   
