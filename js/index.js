35fd763d28265a6072dc023cd11a3ded6625db92
kumu <kumudhini.j@gmail.com>
2021-09-27 17:02:51 +1000

    edited card layout

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 const taskList = new TaskManager();
-// taskList.addTask('Take out the trash', 'Take out the trash to the front of the house', 'Nick', '2020-09-20', 'In Progress');
 console.log(taskList._tasks);
-// const taskHtml = createTaskHtml('validation','validate all fields in form','kumudhini','2020-09-21','completed');
-// console.log(taskHtml);
-
-
 
 // Task 4 - Task Form Validation
 
@@ -25,88 +20,88 @@ const gridRadios = document.getElementsByName('gridRadios');
 const radioErr = document.getElementById('radioErr');
 let count = 0;
 
-    let nameVal = taskName.value;
-    if(nameVal.length < 5 || nameVal === ''){
-        nameErr.innerHTML = 'Name should be at least 5 characters.';
-        nameErr.style.color = 'red';
-        count++;
-    }
-    else{
-        taskName.focus();
-        nameErr.innerHTML = '';
-        
-    }
+let nameVal = taskName.value;
+if(nameVal.length < 5 || nameVal === ''){
+    nameErr.innerHTML = 'Name should be at least 5 characters!';
+    nameErr.style.color = 'red';
+    count++;
+}
+else{
+    taskName.focus();
+    nameErr.innerHTML = '';
+  
+}
 
 
-    let dueDate = duedate.value;
-    if(dueDate === ''){
-        dateErr.innerHTML = 'Date could not be empty.';
-        dateErr.style.color = 'red';
-        count++;
-    }
-    else{
-        // dueDate.focus();
-        dateErr.innerHTML = '';
-        
-    }
-
-    let radioInput;
-    radioValid = false;
-    for(let i=0; i < gridRadios.length; i++){
-        if(gridRadios[i].checked){
-            radioErr.innerHTML = '';
-            radioValid = true;
-            radioInput = gridRadios[i].value;
-        }
-    }
-    if(!radioValid){
-        radioErr.innerHTML = 'Please select the status.'
-        radioErr.style.color = 'red';
-        count++;
-    }
-
-
-
-    let assignTo = assign.value;
-    if(assignTo === ''){
-        assignedErr.innerHTML = 'Name should not be empty!';
-        assignedErr.style.color = 'red';
-        count++;
-    }
-    else{
-        // assignTo.focus();
-        assignedErr.innerHTML = '';
-       
-    }
-
-
-    if(description.value.length < 5 || description.value === null){
-        descriptionErr.innerHTML = 'Please add some description.';
-        descriptionErr.style.color = 'red';
-        count++;
-    }
-    else{
-        descriptionErr.innerHTML = '';
-        
-    }
-
-    if(count > 0){
-    count = 0;
-    return;
-    }
-    else{
-        taskList.addTask(
-            taskName.value,
-            description.value,
-            assign.value,
-            duedate.value,
-            radioInput
-        );
+let dueDate = duedate.value;
+if(dueDate === ''){
+    dateErr.innerHTML = 'Date could not be empty!';
+    dateErr.style.color = 'red';
+    count++;
+}
+else{
+    // dueDate.focus();
+    dateErr.innerHTML = '';
     
-        taskform.reset();
-    
-        taskList.render();
+}
+
+let radioInput;
+radioValid = false;
+for(let i=0; i < gridRadios.length; i++){
+    if(gridRadios[i].checked){
+        radioErr.innerHTML = '';
+        radioValid = true;
+        radioInput = gridRadios[i].value;
     }
+}
+if(!radioValid){
+    radioErr.innerHTML = 'Please select the status!'
+    radioErr.style.color = 'red';
+    count++;
+}
+
+
+
+let assignTo = assign.value;
+if(assignTo === ''){
+    assignedErr.innerHTML = 'Name should not be empty!';
+    assignedErr.style.color = 'red';
+    count++;
+}
+else{
+    // assignTo.focus();
+    assignedErr.innerHTML = '';
+    
+}
+
+
+if(description.value.length < 5 || description.value === null){
+    descriptionErr.innerHTML = 'Please add some description!';
+    descriptionErr.style.color = 'red';
+    count++;
+}
+else{
+    descriptionErr.innerHTML = '';
+    
+}
+
+if(count > 0){
+count = 0;
+return;
+}
+else{
+    taskList.addTask(
+        taskName.value,
+        description.value,
+        assign.value,
+        duedate.value,
+        radioInput
+    );
+
+    taskform.reset();
+
+    taskList.render();
+}
     
 });
 
