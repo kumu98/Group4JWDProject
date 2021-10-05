6253730b9f59e7d5b4d4ecef0ed7de6af1a9a2bb
Denise <deniseleano@gmail.com>
2021-10-05 12:08:34 +1100

    Added Task 9 and 10

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,6 +1,6 @@
 //Card function in html
-function createTaskHtml (id, name, description, assignedTo, dueDate, status) {
-    let html = `<div class="card" data-task-id="${id}" style="width: 18rem">
+function createTaskHtml(id, name, description, assignedTo, dueDate, status) {
+  let html = `<div class="card" data-task-id="${id}" style="width: 18rem">
                 <div class="card-body">
                     <h5 class="card-title">${name}</h5>
                     <p class="card-text">
@@ -13,7 +13,9 @@ function createTaskHtml (id, name, description, assignedTo, dueDate, status) {
                 <li class="list-group-item">${status} </li>
                 </ul>
                 <div class="card-body text-right">
-                    <button class="btn btn-outline-success done-button ${status === "Done" ? "invisible" : "visible"}">
+                    <button class="btn btn-outline-success done-button ${
+                      status === "Done" ? "invisible" : "visible"
+                    }">
                         Done
                     </button>
                     <button class="btn btn-outline-danger delete-button">
@@ -21,84 +23,83 @@ function createTaskHtml (id, name, description, assignedTo, dueDate, status) {
                     </button>
                 </div>
             </div>`;
-return html;
+  return html;
 }
 
 class TaskManager {
-    constructor(currentId=0){
-        this._tasks = [];
-        this._currentId = currentId;
-    } 
-    
-    addTask(name, description, assignedTo, dueDate, status){
-        const newTask = {
-            id: this._currentId++,
-            name: name,
-            description: description,
-            assignedTo: assignedTo,
-            dueDate: dueDate,
-            status: status
-        };
-        this._tasks.push(newTask);
+  constructor(currentId = 0) {
+    this._tasks = [];
+    this._currentId = currentId;
+  }
 
-    }
+  addTask(name, description, assignedTo, dueDate, status) {
+    const newTask = {
+      id: this._currentId++,
+      name: name,
+      description: description,
+      assignedTo: assignedTo,
+      dueDate: dueDate,
+      status: status,
+    };
+    this._tasks.push(newTask);
+  }
 
-    getTaskById(taskId){
-        let foundTask;
-        for(let i = 0; i < this._tasks.length; i++){
-            const task = this._tasks[i];
-            if(task.id === taskId){
-                foundTask = task;
-            }
-        }
-        return foundTask;
+  getTaskById(taskId) {
+    let foundTask;
+    for (let i = 0; i < this._tasks.length; i++) {
+      const task = this._tasks[i];
+      if (task.id === taskId) {
+        foundTask = task;
+      }
     }
+    return foundTask;
+  }
 
-    save() {
-        let tasksJson = JSON.stringify(this._tasks);
-        localStorage.setItem("tasks", tasksJson);
-        let currentId = JSON.stringify(this._currentId);
-        localStorage.setItem ("currentId", currentId);
-    }
+  save() {
+    let tasksJson = JSON.stringify(this._tasks);
+    localStorage.setItem("tasks", tasksJson);
+    let currentId = JSON.stringify(this._currentId);
+    localStorage.setItem("currentId", currentId);
+  }
 
-    load(){
-       if(localStorage.getItem("tasks")){
-        this._tasks = JSON.parse(localStorage.getItem("tasks"));
-       }
-       if(localStorage.getItem("currentId")){
-        this._currentId = JSON.parse(localStorage.getItem("currentId"));
-       }
+  load() {
+    if (localStorage.getItem("tasks")) {
+      this._tasks = JSON.parse(localStorage.getItem("tasks"));
     }
+    if (localStorage.getItem("currentId")) {
+      this._currentId = JSON.parse(localStorage.getItem("currentId"));
+    }
+  }
 
-    deleteTask(taskId){
-        let newTasks = [];
-        for(let i = 0; i < this._tasks.length; i++){
-            const task = this._tasks[i];
-            if(task.id !== taskId){
-               newTasks.push(task);
-            }
-        }
-        this._tasks = newTasks;
+  deleteTask(taskId) {
+    let newTasks = [];
+    for (let i = 0; i < this._tasks.length; i++) {
+      const task = this._tasks[i];
+      if (task.id !== taskId) {
+        newTasks.push(task);
+      }
     }
-    
-    render() {
-       let tasksHtmlList = [];
-       for (let i=0; i < this._tasks.length; i++){
-        const currentTask = this._tasks[i];
-            const taskHtml = createTaskHtml (
-               currentTask.id,
-               currentTask.name,
-               currentTask.description,
-               currentTask.assignedTo,
-               currentTask.dueDate,
-               currentTask.status
-           );
-           
-           tasksHtmlList.push(taskHtml);
-       }
-       const tasksHtml = tasksHtmlList.join("\n");
-       
-       const taskListContainer = document.getElementById('mycard');
-       taskListContainer.innerHTML = tasksHtml;
+    this._tasks = newTasks;
+  }
+
+  render() {
+    let tasksHtmlList = [];
+    for (let i = 0; i < this._tasks.length; i++) {
+      const currentTask = this._tasks[i];
+      const taskHtml = createTaskHtml(
+        currentTask.id,
+        currentTask.name,
+        currentTask.description,
+        currentTask.assignedTo,
+        currentTask.dueDate,
+        currentTask.status
+      );
+
+      tasksHtmlList.push(taskHtml);
     }
+    const tasksHtml = tasksHtmlList.join("\n");
+
+    const taskListContainer = document.getElementById("mycard");
+    taskListContainer.innerHTML = tasksHtml;
+  }
 }
